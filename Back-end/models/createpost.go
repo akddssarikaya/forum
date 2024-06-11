933a6d06ccd33e399d2907ddcecb04c6a19d6118
Emirtariksahin <emirtariik@gmail.com>
2024-06-11 20:06:40 +0300

    createpost kısmında kategori seçenekleri çekildi

--- Back-end/models/createpost.go
+++ Back-end/models/createpost.go
@@ -1,6 +1,11 @@
 package models
 
-import "net/http"
+import (
+	"database/sql"
+	"net/http"
+
+	"forum/handlers"
+)
 
 func HandleCreatePost(w http.ResponseWriter, r *http.Request) {
 	tmpl, ok := tmplCache["create_post"]
@@ -8,14 +13,44 @@ func HandleCreatePost(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Could not load create_post template", http.StatusInternalServerError)
 		return
 	}
+	db, err := sql.Open("sqlite3", "./database/forum.db")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer db.Close()
+
+	// Kategorileri çekmek için sorgu
+	rows, err := db.Query("SELECT id, name FROM categories")
+	if err != nil {
+		http.Error(w, "Could not retrieve categories", http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	var categories []handlers.Category
+	for rows.Next() {
+		var category handlers.Category
+		err := rows.Scan(&category.ID, &category.Name)
+		if err != nil {
+			http.Error(w, "Error scanning category", http.StatusInternalServerError)
+			return
+		}
+		categories = append(categories, category)
+	}
+	if err = rows.Err(); err != nil {
+		http.Error(w, "Error iterating categories", http.StatusInternalServerError)
+		return
+	}
 
 	// Kullanıcı giriş yapmış mı diye kontrol edelim
-	_, err := r.Cookie("user_id")
+	_, err = r.Cookie("user_id")
 	loggedIn := err == nil
 
 	// Şablonu render et
 	data := map[string]interface{}{
-		"LoggedIn": loggedIn,
+		"LoggedIn":   loggedIn,
+		"Categories": categories,
 	}
 
 	// Kullanıcı giriş yapmamışsa Login ve Register bağlantılarını ekle
